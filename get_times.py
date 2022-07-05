29f2f49a74dcd55381156f9046c83148b1f80ee5
Szymon Stanik <stanik.szymon@gmail.com>
2022-07-05 22:02:57 +0200

    Minor fixes

--- get_times.py
+++ get_times.py
@@ -35,32 +35,33 @@ for file_name in files:
 
          if (line[0] == '1'):
             times = line.split("]}")
-            remaining_times[0] = (times[-2].split(" ")[-1])
-            remaining_times[1] = (times[-3].split(" ")[-1])
+            if (len(times) >= 3):
+               remaining_times[0] = (times[-2].split(" ")[-1])
+               remaining_times[1] = (times[-3].split(" ")[-1])
 
-            h,m,s = remaining_times[0].split(':')
-            remaining_times[0] = float(datetime.timedelta(hours=int(h),minutes=int(m),seconds=float(s)).total_seconds())
+               h,m,s = remaining_times[0].split(':')
+               remaining_times[0] = float(datetime.timedelta(hours=int(h),minutes=int(m),seconds=float(s)).total_seconds())
 
-            h,m,s = remaining_times[1].split(':')
-            remaining_times[1] = float(datetime.timedelta(hours=int(h),minutes=int(m),seconds=float(s)).total_seconds())
+               h,m,s = remaining_times[1].split(':')
+               remaining_times[1] = float(datetime.timedelta(hours=int(h),minutes=int(m),seconds=float(s)).total_seconds())
 
-            time_seconds = 2*time_control - remaining_times[0] - remaining_times[1]
+               time_seconds = 2*time_control - remaining_times[0] - remaining_times[1]
 
-            if (added_time != 0):
-               moves[0] = int(times[-2].split('.')[0])
-               moves[1] = int(times[-3].split('.')[0])
-               time_seconds += added_time * (moves[0] + moves[1])
+               if (added_time != 0):
+                  moves[0] = int(times[-2].split('.')[0])
+                  moves[1] = int(times[-3].split('.')[0])
+                  time_seconds += added_time * (moves[0] + moves[1])
 
-            if (time_control > 0):
-               if (time_control < 180):
-                  games_played[0] += 1
-                  month_time_seconds[0] += time_seconds
-               elif (time_control < 600):
-                  games_played[1] += 1
-                  month_time_seconds[1] += time_seconds
-               else:
-                  games_played[2] += 1
-                  month_time_seconds[2] += time_seconds
+               if (time_control > 0):
+                  if (time_control < 180):
+                     games_played[0] += 1
+                     month_time_seconds[0] += time_seconds
+                  elif (time_control < 600):
+                     games_played[1] += 1
+                     month_time_seconds[1] += time_seconds
+                  else:
+                     games_played[2] += 1
+                     month_time_seconds[2] += time_seconds
 
    print(file_name.split(".")[0] + ": " + str(datetime.timedelta(seconds=sum(month_time_seconds))).split('.')[0])
    for i in range(3):
